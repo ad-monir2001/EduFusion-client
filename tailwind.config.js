40d4c02b207cf5283d5bc77ca3ba34a1602f799d
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2025-01-14 08:41:58 +0600

    Environment setup done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'heading': ['"Merriweather", serif'],
+        'body': ['"Lato", serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
