7a4da848506c0ad90fb1eecf48c531329ab47433
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2025-01-16 17:21:59 +0600

    Fetch and display user count in AllUsers component using react-query and secure axios instance

--- src/Components/Dashboard/Admin/AllUsers.jsx
+++ src/Components/Dashboard/Admin/AllUsers.jsx
@@ -1,9 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const AllUsers = () => {
+    const axiosSecure = useAxiosSecure();
+    const {data: users = []} = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/users');
+            return res.data;
+        }
+    })
     return (
         <div>
-            All users
+            All users {users.length}
         </div>
     );
 };
