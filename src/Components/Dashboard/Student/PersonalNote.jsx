eccbb00d72ac43646f745ce66f02a0c56282548e
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2025-01-19 21:45:26 +0600

    Implement update and delete functionality for personal notes; add confirmation modal for updates; enhance user feedback with toast notifications

--- src/Components/Dashboard/Student/PersonalNote.jsx
+++ src/Components/Dashboard/Student/PersonalNote.jsx
@@ -4,10 +4,16 @@ import { useAuth } from '../../../hooks/useAuth';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 import LoadingSpinner from '../../Shared/LoadingSpinner';
+import Swal from 'sweetalert2';
+import { useState } from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const PersonalNote = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
+  const [selectedSession, setSelectedSession] = useState([]);
+  const [updateId, setUpdateId] = useState(null);
 
   const {
     data: notes = [],
@@ -21,21 +27,65 @@ const PersonalNote = () => {
     },
   });
 
-  const noteData = {
-    email: 'ad.monir2001@gmail.com',
-    noteTitle: 'test note',
-    noteDescription:
-      'test note des Progressively facilitate optimal sources and intermanda…',
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+    const updateData = { noteTitle: title, noteDescription: description };
+
+    // update data send to server
+    axios
+      .patch(
+        `${import.meta.env.VITE_API_BASE_URL}/notes/${updateId}`,
+        updateData
+      )
+      .then((response) => {
+        console.log(response.data);
+        toast.success('Successfully updated session status.');
+        refetch();
+        document.getElementById('conform').close();
+      })
+      .catch((error) => {
+        console.log('error to update session', error);
+        toast.error('Failed to update session status.');
+      });
   };
 
-  const handleUpdate = (Id) => {
-    // Implement update logic here
-    console.log(Id);
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'Are you sure to delete this Note?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`${import.meta.env.VITE_API_BASE_URL}/notes/${id}`, {
+          method: 'DELETE',
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: 'Deleted!',
+                text: 'This session has been deleted.',
+                icon: 'success',
+              });
+              refetch();
+            }
+          });
+      }
+    });
   };
 
-  const handleDelete = () => {
-    // Implement delete logic here
-    console.log('Delete clicked for:', noteData.noteTitle);
+  // open session modal
+  const openSessionModal = (noteId) => {
+    const dataToUpdate = notes.filter((data) => data._id === noteId);
+    setSelectedSession(dataToUpdate);
+    setUpdateId(noteId);
+    document.getElementById('conform').showModal();
   };
   return (
     <div>
@@ -83,7 +133,7 @@ const PersonalNote = () => {
               {/* Footer Section with Buttons */}
               <div className=" py-3 bg-gray-50 flex justify-end space-x-2 ">
                 <button
-                  onClick={() => handleUpdate(note._id)}
+                  onClick={() => openSessionModal(note._id)}
                   className="inline-flex items-center px-3 py-2 border border-gray-300 rounded-md text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors"
                 >
                   <FaPencil className="h-4 w-4 mr-2" />
@@ -91,7 +141,7 @@ const PersonalNote = () => {
                 </button>
 
                 <button
-                  onClick={handleDelete}
+                  onClick={() => handleDelete(note._id)}
                   className="inline-flex items-center px-3 py-2 border border-transparent rounded-md text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors"
                 >
                   <LuTrash2 className="h-4 w-4 mr-2" />
@@ -102,6 +152,66 @@ const PersonalNote = () => {
           ))}
         </div>
       )}
+
+      {/* show the conformation modal */}
+      <dialog id="conform" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box text-center">
+          <h3 className="font-bold text-xl font-heading text-[#ff3600]">
+            Update the session rejected or approved?
+          </h3>
+          <div className="flex items-center justify-between border p-2 rounded-xl my-2 ">
+            {selectedSession.map((data) => (
+              <form
+                onSubmit={handleUpdate}
+                key={data._id}
+                className="card-body"
+              >
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-heading">Note Title</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="title"
+                    defaultValue={data.noteTitle}
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-heading">
+                      Note Description
+                    </span>
+                  </label>
+                  <textarea
+                    type="text"
+                    name="description"
+                    defaultValue={data.noteDescription}
+                    rows={6}
+                    className="w-full px-4 py-2 border rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    required
+                  />
+                </div>
+                <button className="btn btn-success font-heading text-white my-2">
+                  Confirm update Note
+                </button>
+              </form>
+            ))}
+          </div>
+          <div className="modal-action">
+            <form
+              method="dialog"
+              className="flex justify-between gap-4 items-center"
+            >
+              <button className="btn btn-error text-white font-heading">
+                Cancel
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
