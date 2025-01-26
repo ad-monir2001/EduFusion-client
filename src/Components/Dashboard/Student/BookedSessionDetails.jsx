e388e1bdadf0979f1245625c032b9016ae352798
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2025-01-26 11:42:12 +0600

    Add review submission feature in BookedSessionDetails; implement form handling and toast notifications

--- src/Components/Dashboard/Student/BookedSessionDetails.jsx
+++ src/Components/Dashboard/Student/BookedSessionDetails.jsx
@@ -2,10 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import { useAuth } from '../../../hooks/useAuth';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const BookedSessionDetails = () => {
   const params = useParams();
-  console.log(params.id);
   const { user } = useAuth();
 
   const axiosSecure = useAxiosSecure();
@@ -24,6 +25,36 @@ const BookedSessionDetails = () => {
   const desiredSession = bookedSessions.filter(
     (session) => session._id === params.id
   );
+
+  const handleReview = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const studentName = form.name.value;
+    const studentEmail = form.email.value;
+    const review = form.review.value;
+    const rating = parseInt(form.rating.value);
+    const sessionId = desiredSession[0].sessionId;
+    const reviewDetails = {
+      studentName,
+      studentEmail,
+      review,
+      rating,
+      sessionId,
+    };
+
+    // send data to server
+    axios
+      .post(`${import.meta.env.VITE_API_BASE_URL}/reviews`, reviewDetails)
+      .then((response) => {
+        console.log(response.data);
+        toast.success('Successfully Accepted your Reviews');
+
+        form.reset();
+      })
+      .catch((error) => {
+        console.log('error to Submit Review', error.message);
+      });
+  };
   return (
     <div>
       {desiredSession.map((session) => (
@@ -41,7 +72,7 @@ const BookedSessionDetails = () => {
           </p>
           <p className="text-gray-700 mb-4 font-body">{session.description}</p>
           <div className="flex flex-col md:flex-row gap-4 justify-between items-start">
-            <div className='px-6'>
+            <div className="px-6">
               <p className=" text-gray-600 font-body">
                 <span className="font-semibold">Student Name:</span>{' '}
                 {session.studentName}
@@ -59,7 +90,7 @@ const BookedSessionDetails = () => {
                 {session.duration}
               </p>
             </div>
-            <div className='px-6'>
+            <div className="px-6">
               <p className=" text-gray-600">
                 <span className="font-semibold font-body">Student Email:</span>{' '}
                 {session.studentEmail}
@@ -76,8 +107,102 @@ const BookedSessionDetails = () => {
           </div>
         </div>
       ))}
-      <div>
-        
+
+      {/* Review and ratings */}
+
+      <div className="mt-14">
+        <div className="text-center">
+          <h1 className="font-heading text-3xl">
+            Provide Your Review and Ratings
+          </h1>
+          <p className="font-body">
+            Share your feedback and rate your experience to help us improve.
+            Your insights ensure better study sessions and a more collaborative
+            learning environment!
+          </p>
+        </div>
+        {desiredSession.map((session) => (
+          <form onSubmit={handleReview} className="space-y-6" key={session._id}>
+            {/* Email Field */}
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700 mb-2"
+              >
+                Student Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                readOnly
+                defaultValue={user.email}
+                className="w-full px-4 py-2 border rounded-lg cursor-not-allowed bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            {/* name Field */}
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700 mb-2"
+              >
+                Student Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                readOnly
+                defaultValue={session.studentName}
+                className="w-full cursor-not-allowed px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Enter note title"
+              />
+            </div>
+
+            {/* review Field */}
+            <div>
+              <label
+                htmlFor="review"
+                className="block text-sm font-medium text-gray-700 mb-2"
+              >
+                Your Review
+              </label>
+              <textarea
+                id="review"
+                required
+                name="review"
+                rows={4}
+                className="w-full px-4 py-2 border rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Enter Review..."
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="rating"
+                className="block text-sm font-medium text-gray-700 mb-2"
+              >
+                Your Rating
+              </label>
+              <input
+                type="number"
+                id="rating"
+                name="rating"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Your Rating (1-5)"
+              />
+            </div>
+
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="w-full font-heading bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            >
+              Send Review and Rating
+            </button>
+          </form>
+        ))}
       </div>
     </div>
   );
